4c1b784395d778b17672503f95784e9caee78f73
soymario08@gmail.com <soymario08@gmail.com@388dde58-b828-ffd0-8e80-97a74cba5b35>
2011-05-25 15:50:15 +0000

    Cambiado a version 2.5 compatible con Apache 6

--- isg3-democraciadirecta/src/presentation/FrontController.java
+++ isg3-democraciadirecta/src/presentation/FrontController.java
@@ -3,7 +3,6 @@ package presentation;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +15,6 @@ import domain.Usuario;
 /**
  * Servlet implementation class FrontController
  */
-@WebServlet("/FrontController")
 public class FrontController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
