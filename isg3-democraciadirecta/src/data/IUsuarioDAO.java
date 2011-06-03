fb2cf23849673ac80587ea7b20fbf66988bdf0ca
pakopv@gmail.com <pakopv@gmail.com@388dde58-b828-ffd0-8e80-97a74cba5b35>
2011-06-03 17:33:01 +0000

    Agregado método selectAll()

--- isg3-democraciadirecta/src/data/IUsuarioDAO.java
+++ isg3-democraciadirecta/src/data/IUsuarioDAO.java
@@ -9,14 +9,16 @@ public interface IUsuarioDAO {
 
 	Usuario select(String idUsuario);
 	
-	boolean add(Usuario u);
+	List<Usuario> selectAll();
 	
-	Usuario delete(Usuario u);
-
 	boolean valida(String nick, String passwd);
 
 	Usuario select(String nick, String passwd);
 	
+	boolean add(Usuario u);
+	
+	Usuario delete(Usuario u);
+
 	List<Tag> selectTags(Usuario user);
 	
 	boolean addTag(Tag t);
