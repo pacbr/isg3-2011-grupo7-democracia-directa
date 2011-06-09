7ac7b0a3bbf2d2941d750b4354039f4203df7281
pakopv@gmail.com <pakopv@gmail.com@388dde58-b828-ffd0-8e80-97a74cba5b35>
2011-06-09 18:38:51 +0000

    Ya se pueden votar las leyes, falta controlar que no pueda votar más de una vez el mismo usuario una pley

--- isg3-democraciadirecta/src/domain/ProccesorUsuario.java
+++ isg3-democraciadirecta/src/domain/ProccesorUsuario.java
@@ -1,5 +1,25 @@
 package domain;
 
-public class ProccesorUsuario implements IProccesorUsuario{
+import data.IPLeyDAO;
+import data.IUsuarioDAO;
+import data.JDBCPLeyDAO;
+import data.JDBCUsuarioDAO;
 
+public class ProccesorUsuario implements IProccesorUsuario{
+IUsuarioDAO usuarioDAO = new JDBCUsuarioDAO();
+IPLeyDAO pleyDAO = new JDBCPLeyDAO();
+	
+	@Override
+	public boolean votaPLey(PLey p, Usuario u){
+		boolean res = false;
+		res = usuarioDAO.insertVoto(p.getId(), u.getId());
+		res = res && pleyDAO.insertVoto(p.getId());
+		return res;
+	}
+
+	@Override
+	public boolean pleyVotada(Usuario usuario, PLey pley) {
+		// TODO PACO
+		return false;
+	}
 }
