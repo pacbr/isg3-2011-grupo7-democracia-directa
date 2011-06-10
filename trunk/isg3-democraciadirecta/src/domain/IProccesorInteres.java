cac2bd740e4ef278fa1580bda09ce8fa28e7ecfd
pakopv@gmail.com <pakopv@gmail.com@388dde58-b828-ffd0-8e80-97a74cba5b35>
2011-06-10 20:25:28 +0000

    Caso de uso pleyes interesantes terminado

--- trunk/isg3-democraciadirecta/src/domain/IProccesorInteres.java
+++ trunk/isg3-democraciadirecta/src/domain/IProccesorInteres.java
@@ -4,14 +4,9 @@ import java.util.List;
 import java.util.SortedMap;
 
 public interface IProccesorInteres {
-	List<Usuario> getUsers();
-	SortedMap<Integer, List<PLey>> getPleysInteresantesByUser(Usuario user);
-	Integer getInteresByUser(Usuario user);
-	List<PLey> getPleysNoActivasByUser(Usuario user);
-	List<PLey> getPLeysActivas();
 	Integer getInteresUsuario(Usuario u);
 	int getMovimientoEnLista(PLey p, int actualPos);
 	void setNewMaxPositionOnList(PLey p, int posicionLista);
 	void setNewMinPositionOnList(PLey p, int posicionLista);
-	SortedMap<Integer,List<PLey>> creaMapaPLeyes(List<PLey> pleyesActivas);
+	SortedMap<Integer,List<PLey>> creaMapaPLeyes();	
 }
